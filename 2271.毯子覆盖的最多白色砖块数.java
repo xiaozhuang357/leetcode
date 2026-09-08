142d0613a79f28059543272f4a0f4c9defdec0d7
zhang lishuo <3298211433@qq.com>
2026-09-08 22:19:10 +0800

    优化 2271.毯子覆盖的最多白色砖块数 解法

--- 2271.毯子覆盖的最多白色砖块数.java
+++ 2271.毯子覆盖的最多白色砖块数.java
@@ -11,7 +11,22 @@ import java.util.Arrays;
 class Solution {
     public int maximumWhiteTiles(int[][] tiles, int carpetLen) {
         Arrays.sort(tiles, (a, b) -> a[0] - b[0]);
-        
+        int ans = 0;
+        int left = 0;
+        int cover = 0;
+        for (int i = 0; i < tiles.length; i++) {
+            int tl = tiles[i][0];
+            int tr = tiles[i][1];
+            cover += tr - tl + 1;
+            int carperLeft = tr - carpetLen + 1;
+            while (tiles[left][1] < carperLeft) {
+                cover -= tiles[left][1] - tiles[left][0] + 1;
+                left++;
+            }
+            int uncover = Math.max(carperLeft - tiles[left][0], 0);
+            ans = Math.max(ans, cover - uncover);
+        }
+        return ans;
     }
 }
 // @lc code=end
