dce7218d3dc2d129b39a915ee31b5275fc3853a6
Lishuo <3298211433@qq.com>
2026-09-24 19:40:36 +0800

    提供2300.咒语和药水的成功对数 的双指针解法，但是效果不如二分

--- 2300.咒语和药水的成功对数.java
+++ 2300.咒语和药水的成功对数.java
@@ -11,34 +11,28 @@ import java.util.Arrays;
 class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         int n = spells.length;
+        int m = potions.length;
         int[] ans = new int[n];
 
-        Arrays.sort(potions);
-
+        int[][] pairs = new int[n][2];
         for (int i = 0; i < n; i++) {
-            int index = 0;
-            if (spells[i] < success) {
-                index = lowerBound(potions, success, spells[i]);
-            }
-            ans[i] = potions.length - index;
+            pairs[i][0] = spells[i];
+            pairs[i][1] = i;
         }
+        Arrays.sort(potions);
+        Arrays.sort(pairs, (a, b) -> (a[0] - b[0]));
+
+        int j = m - 1;
+        for (int[] pair : pairs) {
+            int spell = pair[0];
+            int index = pair[1];
+            while (j >= 0 && (long) potions[j] * spell >= success) {
+                j--;
+            }
+            ans[index] = m - j - 1;
+        }
+
         return ans;
-    }
-
-    private int lowerBound(int[] arr, long target, int spell) {
-        int left = 0;
-        int right = arr.length;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if ((long) arr[mid] * spell < target) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
     }
 }
 // @lc code=end
